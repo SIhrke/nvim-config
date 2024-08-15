b73276d26245f9fefa73d7cf4368caeb90896708
Stephan Ihrke <stephan.ihrke@anvajo.com>
2024-08-15 18:57:14 +0200

    add clang format

--- lua/plugin_config/mason.lua
+++ lua/plugin_config/mason.lua
@@ -96,4 +96,11 @@ mason_lspconfig.setup_handlers {
   end,
 }
 
+-- Autoformat on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = { "*.c", "*.cpp", "*.h", "*.hpp" },
+  callback = function()
+    vim.lsp.buf.format({ async = false })
+  end,
+})
 
